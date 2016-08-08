eb3e6340aab83d79d25bcac7c351e93adeabae60
Samantha Spensley <spenslsm@gmail.com>
2016-08-08 16:34:01 -0600

    more https firebase issues fixed

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 
     var artist = $("#artist").val();
     var location = $("#location").val();
-    var url = 'https://cors-anywhere.herokuapp.com/http://api.bandsintown.com/artists/' + artist + '/events/recommended?location=' + location + '&radius=150&app_id=concertgen&api_version=2.0&format=json';
+    var url = 'https://cors-anywhere.herokuapp.com/https://api.bandsintown.com/artists/' + artist + '/events/recommended?location=' + location + '&radius=150&app_id=concertgen&api_version=2.0&format=json';
 
     $.get(url)
       .then(function (data){
